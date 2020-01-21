db217fd6722fbf5fa985f198b4d6c1ea91907661
ryan.c <jackblackevo@gmail.com>
2020-01-21 12:50:32 +0800

    feat(index): add global style, reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import store from './app/store';
 import App from './app/App';
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+    font-family: 'HelveticaNeue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+
+    background: #fbc856;
+  }
+
+  body {
+    width: 100vw;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`;
+
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <>
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>,
   document.getElementById('root')
 );
