0867e83d8c93e3e3bf533620ae24cd5de2cd53d8
ryan.c <jackblackevo@gmail.com>
2020-01-30 08:53:35 +0800

    Updates

---
+++ precache-manifest.a9ecdaf25647d8ffddd1626c4b9c49f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2e909fefa96af3687630bfc596c38a",
+    "url": "/react-order-board/index.html"
+  },
+  {
+    "revision": "b6988fb71d64fcbf0342",
+    "url": "/react-order-board/static/js/2.f5ca4dbd.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/react-order-board/static/js/2.f5ca4dbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "941242fa981f82ee515c",
+    "url": "/react-order-board/static/js/main.1c621506.chunk.js"
+  },
+  {
+    "revision": "6e133769afd66e65fbd1",
+    "url": "/react-order-board/static/js/runtime-main.18174296.js"
+  }
+]);
